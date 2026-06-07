c71a5966b3ebfa3f06ceee785c19a5cfce9f0687
Malik-Zohaib-Debug <zohaibzahid112233@gmail.com>
2026-06-07 23:57:00 +0500

    added assets and bottom navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,44 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
-      <Link href="/onboarding">Onboarding</Link>
-      <Link href="/(auth)/sign-in">Login</Link>
-      <Link href="/(auth)/sign-out">Create an account</Link>
-
       <Link
+        href="/onboarding"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Onboarding
+      </Link>
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Sign in
+      </Link>
+      <Link
+        href="/(auth)/sign-out"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Sign up
+      </Link>
+
+      {/* <Link
         href={{
           pathname: "/(tabs)/subscriptions/[id]",
           params: { id: "claude" },
         }}
       >
         Claude Max Subscription
-      </Link>
-    </View>
+      </Link> */}
+    </SafeAreaView>
   );
 }
